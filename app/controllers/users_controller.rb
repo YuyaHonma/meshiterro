e1b735a809069db609b19e67d0a0709eeea6bd2f
EC2 Default User <0311.hnmyuya@gmail.com>
2023-12-16 17:59:26 +0000

    16章【ユーザーに関する機能(3) - ユーザー情報の編集】

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,23 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "プロフィールが更新されました"
+      redirect_to user_path(@user) # ユーザーの詳細ページへのパス
+    else
+      render 'edit'
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
