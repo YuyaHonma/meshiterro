888e3fede07aba6fcd8a078c0f0de7a913d891a9
EC2 Default User <0311.hnmyuya@gmail.com>
2023-12-16 16:10:44 +0000

    11章【投稿機能の作成(4) - 投稿一覧を見れるようにしよう】

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  get "/about" => "homes#about", as: "about"
 end
